c1da8ea5bad083242c3f87fe04f6d12f84a72484
1212222222 <topcusinan886@gmail.com>
2026-07-10 16:02:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ Botun şu anki durumu:
   let response
   try {
     // API Sürümü gemini-2.0-flash olarak güncellendi.
-    response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`, {
+    response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${GEMINI_API_KEY}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
