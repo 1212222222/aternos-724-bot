e3cf02d1f203a5abc15784725a5592835e17f20c
1212222222 <topcusinan886@gmail.com>
2026-07-10 07:54:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ async function beyinIslemcisi(oyuncuMesaji, gonderenOyuncu) {
         `;
 
         const response = await ai.models.generateContent({
-            model: 'gemini-2.5-flash-latest',
+            model: 'gemini-3.5-flash',
             contents: oyuncuMesaji,
             config: {
                 systemInstruction: sistemTalimati,
